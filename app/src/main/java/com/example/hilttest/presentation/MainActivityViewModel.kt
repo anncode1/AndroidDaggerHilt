082fa94a34a1157c4bc217d75b4c004b3f6be53f
Anahi Salgado <anahisalgado.diaz@gmail.com>
2020-07-28 03:24:06 -0500

    @ViewModelInject used in MainActivityViewModel.kt to inject GetCatRandomFact

---
+++ app/src/main/java/com/example/hilttest/presentation/MainActivityViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.hilttest.presentation
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.hilttest.domain.catfacts.model.Fact
+import com.example.hilttest.domain.catfacts.usecases.GetCatRandomFact
+/**
+ * Created by anahi.salgado on 28/07/2020
+ */
+class MainActivityViewModel @ViewModelInject constructor(
+    private val getCatRandomFact: GetCatRandomFact
+): ViewModel() {
+
+    private val catFact = MutableLiveData<CatFactStates>()
+    fun getCatFact(): LiveData<Fact> {
+        loadingState()
+        val fact: Fact = getCatRandomFact.invoke().isSuccessful
+        catFactState(fact)
+    }
+
+    private fun loadingState() {
+        catFact.value = CatFactStates.Loading
+    }
+
+    private fun catFactState(fact: Fact) {
+        catFact.value = CatFactStates.CatFactData(fact)
+    }
+}
