a00dfeec9b00fb35477c13e63442a6a3d0ff1c32
Anahi Salgado <anahisalgado.diaz@gmail.com>
2020-07-28 02:10:13 -0500

    moving to methods

--- app/src/main/java/com/example/hilttest/MainActivity.kt
+++ app/src/main/java/com/example/hilttest/MainActivity.kt
@@ -17,17 +17,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val contactsStringBuilder = StringBuilder()
-        contacts.forEach {
-            contactsStringBuilder.append(it)
-        }
-        tvLabelContacts.text = contactsStringBuilder
+        showContactList()
+        showAgenda()
+    }
 
+    private fun showAgenda() {
         val agendaStringBuilder = StringBuilder()
         agendaStringBuilder.append("Type: ${agenda.type}\n")
         agenda.contacts.forEach {
             agendaStringBuilder.append(it)
         }
         tvLabelAgenda.text = agendaStringBuilder
+    }
+
+    private fun showContactList() {
+        val contactsStringBuilder = StringBuilder()
+        contacts.forEach {
+            contactsStringBuilder.append(it)
+        }
+        tvLabelContacts.text = contactsStringBuilder
     }
 }
