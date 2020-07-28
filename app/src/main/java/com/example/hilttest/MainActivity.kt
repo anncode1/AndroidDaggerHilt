cddb487b38a7ce51821a6ef9ddb6cf5869fc86e4
Anahi Salgado <anahisalgado.diaz@gmail.com>
2020-07-28 01:03:00 -0500

    injecting contact list in MainActivity and printing data in textview

--- app/src/main/java/com/example/hilttest/MainActivity.kt
+++ app/src/main/java/com/example/hilttest/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.hilttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hilttest.domain.model.Contact
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject lateinit var contacts: List<Contact>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val contactsStringBuilder = StringBuilder()
+        contacts.forEach {
+            val contactFormat = "Name: ${it.name}, Phone: ${it.phone}\n"
+            contactsStringBuilder.append(contactFormat)
+        }
+        tvLabel.text = contactsStringBuilder
     }
 }
--- app/src/main/java/com/example/hilttest/domain/model/Contact.kt
+++ app/src/main/java/com/example/hilttest/domain/model/Contact.kt
@@ -4,6 +4,6 @@ package com.example.hilttest.domain.model
  * Created by anahi.salgado on 27/07/2020
  */
 data class Contact(
-    private val name: String,
-    private val phone: String
+    val name: String,
+    val phone: String
 )
