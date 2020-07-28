8378aa8bc6a46900e9e094659fe34a71bba486ce
Anahi Salgado <anahisalgado.diaz@gmail.com>
2020-07-27 23:03:43 -0500

    HiltAndroidApp first annotation

--- app/src/main/java/com/example/hilttest/MyApplication.kt
+++ app/src/main/java/com/example/hilttest/MyApplication.kt
@@ -1,8 +1,10 @@
 package com.example.hilttest
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 /**
  * Created by anahi.salgado on 27/07/2020
  */
+@HiltAndroidApp
 open class MyApplication: Application()
