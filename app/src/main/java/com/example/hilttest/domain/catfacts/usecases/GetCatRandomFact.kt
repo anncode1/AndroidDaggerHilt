240fd4a1fe8683d78f18c5c31c67585bb81dbce5
Anahi Salgado <anahisalgado.diaz@gmail.com>
2020-07-28 03:23:04 -0500

    use case layer

---
+++ app/src/main/java/com/example/hilttest/domain/catfacts/usecases/GetCatRandomFact.kt
@@ -0,0 +1,12 @@
+package com.example.hilttest.domain.catfacts.usecases
+
+import com.example.hilttest.domain.catfacts.model.Fact
+import retrofit2.Call
+import retrofit2.Response
+
+/**
+ * Created by anahi.salgado on 28/07/2020
+ */
+interface GetCatRandomFact {
+    fun invoke(): Response<Fact>
+}
---
+++ app/src/main/java/com/example/hilttest/domain/catfacts/usecases/GetCatRandomFactImpl.kt
@@ -0,0 +1,13 @@
+package com.example.hilttest.domain.catfacts.usecases
+
+import com.example.hilttest.domain.catfacts.model.Fact
+import com.example.hilttest.domain.catfacts.repository.CatFactRepository
+import retrofit2.Response
+
+/**
+ * Created by anahi.salgado on 28/07/2020
+ */
+class GetCatRandomFactImpl(private val catFactRepository: CatFactRepository):
+    GetCatRandomFact {
+    override fun invoke(): Response<Fact> = catFactRepository.getCatRandomFact()
+}
