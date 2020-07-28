a6df38b71060aa9b0f516b1f6d6140811a788675
Anahi Salgado <anahisalgado.diaz@gmail.com>
2020-07-27 23:53:18 -0500

    models created

---
+++ app/src/main/java/com/example/hilttest/domain/model/Agenda.kt
@@ -0,0 +1,8 @@
+package com.example.hilttest.domain.model
+
+/**
+ * Created by anahi.salgado on 27/07/2020
+ */
+data class Agenda(
+    private val contacts: List<Contact>
+)
---
+++ app/src/main/java/com/example/hilttest/domain/model/Contact.kt
@@ -0,0 +1,9 @@
+package com.example.hilttest.domain.model
+
+/**
+ * Created by anahi.salgado on 27/07/2020
+ */
+data class Contact(
+    private val name: String,
+    private val phone: String
+)
