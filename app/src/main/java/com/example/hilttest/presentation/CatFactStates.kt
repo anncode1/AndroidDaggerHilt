19cba440494a18d1d22ce771a61aa949a5899e14
Anahi Salgado <anahisalgado.diaz@gmail.com>
2020-07-28 03:23:29 -0500

    CatFactStates sealed class presentation layer

---
+++ app/src/main/java/com/example/hilttest/presentation/CatFactStates.kt
@@ -0,0 +1,11 @@
+package com.example.hilttest.presentation
+
+import com.example.hilttest.domain.catfacts.model.Fact
+
+/**
+ * Created by anahi.salgado on 28/07/2020
+ */
+sealed class CatFactStates {
+    object Loading: CatFactStates()
+    data class CatFactData(val fact: Fact): CatFactStates()
+}
